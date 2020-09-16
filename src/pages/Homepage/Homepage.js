6d1e3353e84bea10d0811ae09871e0f90464b45c
timlaurijs <timlaurijs@gmail.com>
2020-09-16 15:58:34 +0200

    added components to Homepage.js

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,9 +1,21 @@
 import React from "react"
 
+//components
+import WorkIndex from "../../components/WorkIndex/WorkIndex"
+import VideoPlayer from "../../components/VideoPlayer/VideoPlayer"
+import Images from "../../components/Images/Images"
+import BodyText from "../../components/BodyText/BodyText"
+import Title from "../../components/Title/Title"
+
 const Homepage = () => {
   return (
     <div className="Homepage">
       <h1>Works</h1>
+      <Title />
+      <WorkIndex />
+      <VideoPlayer />
+      <Images />
+      <BodyText />
     </div>
   )
 }
