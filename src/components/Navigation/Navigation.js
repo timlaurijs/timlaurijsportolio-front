e0680f21c3c53e9ef0242e149654d6a67700ceb0
timlaurijs <timlaurijs@gmail.com>
2020-09-30 16:39:40 +0200

    added active style to Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,19 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 import { NavLink } from "react-router-dom"
+
+// styles
+import "./Navigation.scss"
 
 const Navigation = () => {
   return (
-    <div className="Navlink">
-      <NavLink exact to="/">
+    <div className="Navigation">
+      <NavLink exact to="/" activeClassName="active">
         Work
       </NavLink>
-      <NavLink to="/about">About</NavLink>
+      <NavLink to="/about" activeClassName="active">
+        About
+      </NavLink>
     </div>
   )
 }
