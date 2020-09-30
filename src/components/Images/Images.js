b669c91a52be4435847e7d922ba4b8d2cc09a14f
timlaurijs <timlaurijs@gmail.com>
2020-09-30 16:39:11 +0200

    created image browser w basic styling

--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -1,22 +1,45 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 //store
-import { selectImages } from "../../App/App-selectors"
+import { selectImages, getCurrentPost } from "../../App/App-selectors"
 //style
 import "./Images.scss"
 
 const Images = () => {
   const { slug } = useParams()
-  const imageUrls = useSelector(selectImages(slug))
+  const imageUrls = useSelector(selectImages)
+  const [selectedImage, setSelectedImage] = useState("")
 
-  // console.log("imageUrls", imageUrls)
+  // sets selectedImage fot first of the array if empty
+  useEffect(() => {
+    if (imageUrls && selectedImage === "") setSelectedImage(imageUrls[0])
+  }, [imageUrls, selectedImage, slug])
+
+  // setsSelected image if another work is clicked
+  useEffect(() => {
+    if (imageUrls && imageUrls.includes(selectedImage)) return
+    else if (imageUrls) setSelectedImage(imageUrls[0])
+    else setSelectedImage("")
+  }, [imageUrls, selectedImage])
 
   return (
-    <div>
-      {imageUrls.map((image, i) => (
-        <img key={i} src={image} />
-      ))}
+    <div className="Images">
+      <div className="Images__nav">
+        {imageUrls &&
+          imageUrls.map((image, i) => (
+            <div
+              className="Images__nav__buttons"
+              key={i}
+              onClick={() => setSelectedImage(image)}
+              style={{ backgroundImage: `url(${image})` }}
+            />
+          ))}
+      </div>
+
+      <div className="Images__selected">
+        <img src={selectedImage}></img>
+      </div>
     </div>
   )
 }
