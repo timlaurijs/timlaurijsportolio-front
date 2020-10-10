28ad2c55884dcc0542ac9b7f575a2192a2f011bc
timlaurijs <timlaurijs@gmail.com>
2020-10-10 07:07:45 +0200

    added media player in VideoPlayer

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { useSelector } from "react-redux"
+import { Media, Player, controls } from 'react-media-player'
+
 //store
 import { selectVideos } from "../../App/App-selectors"
 //style
@@ -7,18 +9,24 @@ import "./VideoPlayer.scss"
 
 const VideoPlayer = () => {
   const videoUrls = useSelector(selectVideos)
+  const { PlayPause, MuteUnmute } = controls
 
   return (
     <div className="VideoPlayer">
-      {/* {videoUrls.map((video, i) => (
-        <iframe
-          key={i}
-          width="420"
-          height="315"
-          src={videoUrls}
-          // ref={(ref) => console.dir(ref)}
-        ></iframe>
-      ))} */}
+        <Media className="VideoPlayer__selected">
+        <div className="VideoPlayer__selected">
+          <div className="media-player">
+            <Player src="http://www.youtube.com/embed/h3YVKTxTOgU" />
+          </div>
+          <div className="media-controls">
+            <PlayPause />
+            <MuteUnmute />
+          </div>
+        </div>
+      </Media>
+      <div>
+        
+      </div>
     </div>
   )
 }
