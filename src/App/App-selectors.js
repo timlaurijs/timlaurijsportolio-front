ab596f910d92fec016061e6091949729e033ef53
timlaurijs <timlaurijs@gmail.com>
2020-10-22 15:49:31 +0200

    destructure yt link  in selector

--- src/App/App-selectors.js
+++ src/App/App-selectors.js
@@ -1,18 +1,18 @@
+import getYoutubeId from "get-youtube-id"
+
 export const selectError = (state) => state.App.error
 
 export const selectAllPosts = (state) => state.App.posts
 
-export const getCurrentPost = (state) => state.App.currentPost &&
-    state.App.currentPost
-
-
+export const getCurrentPost = (state) =>
+  state.App.currentPost && state.App.currentPost
 
 export const selectTitles = (state) =>
   Object.values(state.App.posts).map(({ title, slug, id, publishedAt }) => ({
     title,
     slug,
     id,
-    publishedAt: new Date(publishedAt).getFullYear()
+    publishedAt: new Date(publishedAt).getFullYear(),
   }))
 
 export const selectTitle = (state) =>
@@ -22,11 +22,11 @@ export const selectBodyText = (state) =>
   state.App.currentPost.body && state.App.currentPost.body
 
 export const selectVideos = (state) =>
-  state.App.currentPost.video &&
-  state.App.currentPost.video.map((video) => video.url)
+  state.App.currentPost.videos &&
+  state.App.currentPost.videos.map((video) => getYoutubeId(video.url))
 
 export const selectImages = (state) =>
   state.App.currentPost.images &&
   state.App.currentPost.images.map((image) => image.url)
 
-  export const selectMediaType = (state) => state.App.mediaType
+export const selectMediaType = (state) => state.App.mediaType
